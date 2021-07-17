3cd0010bdc6cca1cbc0bb03c73ee8b9503c6ed73
liujieboss <3028131548@qq.com>
2021-07-17 22:01:54 +0800

    first commit

---
+++ src/main/java/com/liujie/gitxixixixi/GitXixixixiApplication.java
@@ -0,0 +1,13 @@
+package com.liujie.gitxixixixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitXixixixiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitXixixixiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liujie/gitxixixixi/GitXixixixiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liujie.gitxixixixi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitXixixixiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
